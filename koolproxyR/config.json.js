c9aaacde1e89e46b34238b153dfd61dc437d5a37
Xu Zhaowei <paldier@hotmail.com>
2019-09-20 13:18:39 +0800

    add kpr

---
+++ koolproxyR/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.0.0",
+"md5":"e2b595f0cdab9042289988d04b180adf",
+"home_url":"Module_koolproxyR.asp",
+"title":"koolproxyR",
+"description":"KPR更多规则更舒服！",
+"build_date":"2019-09-20_11:40:02"
+}
