31765ea1342a4c0568f6f3f2aa346dbba20dada0
Xu Zhaowei <paldier@hotmail.com>
2020-02-13 00:22:03 +0800

    update

---
+++ tenddns/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"0.0.1",
+"md5":"f229c8ea5ffe57d99364cba9b800d7f1",
+"home_url":"Module_tenddns.asp",
+"title":"腾讯云ddns",
+"description":"腾讯云ddns",
+"build_date":"2020-01-06_18:24:10"
+}
