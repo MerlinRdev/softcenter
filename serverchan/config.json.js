7ce708a56edc4d4a47daab20ae1aaedb5e6b3fee
Xu Zhaowei <paldier@hotmail.com>
2020-03-20 22:01:27 +0800

    update serverchan

--- serverchan/config.json.js
+++ serverchan/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.5",
-"md5":"1ec24a5597794bca4567f7be8f944370",
+"version":"1.6",
+"md5":"13b61faeafb8a2db6298373e288635a8",
 "home_url":"Module_serverchan.asp",
 "title":"ServerChan微信推送",
 "description":"从路由器推送状态及通知的工具",
-"build_date":"2020-03-10_12:35:05"
+"build_date":"2020-03-20_22:01:19"
 }
