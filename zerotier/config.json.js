3a89f22375102ed5616166c09f1f60266752e0a8
Xu Zhaowei <paldier@hotmail.com>
2021-01-24 11:18:45 +0800

    fix

--- zerotier/config.json.js
+++ zerotier/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"0.2",
-"md5":"59956a0e24a723d812028933ce464969",
+"version":"0.3",
+"md5":"e442868a5660250a0e03d875c271a7dd",
 "home_url":"Module_zerotier.asp",
 "title":"zerotier",
 "description":"分布式的虚拟以太网",
-"build_date":"2021-01-15_11:44:04"
+"build_date":"2021-01-24_11:17:55"
 }
