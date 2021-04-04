8017acc687f25fada9c02968fc1eaf7f5bd4af2b
Xu Zhaowei <paldier@hotmail.com>
2021-04-04 17:09:52 +0800

    update verysync

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -131,14 +131,14 @@
             "version": "0.0.3"
         }, 
         {
-            "build_date": "2020-08-13_12:01:16", 
+            "build_date": "2021-04-04_17:09:01", 
             "description": "自己的私有云", 
             "home_url": "Module_verysync.asp", 
-            "md5": "da9c9492728205c6dc6a8751d66e385e", 
+            "md5": "2fa32c61b65aa7784d60181a82f00370", 
             "name": "verysync", 
             "tar_url": "verysync/verysync.tar.gz", 
             "title": "微力同步", 
-            "version": "1.0.1"
+            "version": "1.1.0"
         }, 
         {
             "build_date": "2020-08-13_09:59:22", 
--- verysync/config.json.js
+++ verysync/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.0.1",
-"md5":"da9c9492728205c6dc6a8751d66e385e",
+"version":"1.1.0",
+"md5":"2fa32c61b65aa7784d60181a82f00370",
 "home_url":"Module_verysync.asp",
 "title":"微力同步",
 "description":"自己的私有云",
-"build_date":"2020-08-13_12:01:16"
+"build_date":"2021-04-04_17:09:01"
 }
