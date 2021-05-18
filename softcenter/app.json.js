8b8a939e0e73a7344800f98ff9228d1f40b20e02
Xu Zhaowei <paldier@hotmail.com>
2021-05-18 09:23:40 +0800

    fix unblockmusic

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,14 +1,14 @@
 {
     "apps": [
         {
-            "build_date": "2021-05-08_16:05:27", 
+            "build_date": "2021-05-18_09:23:31", 
             "description": "解锁网易云灰色歌曲", 
             "home_url": "Module_unblockmusic.asp", 
-            "md5": "67402f67050552719232653668e7346b", 
+            "md5": "e144b391bf6dbabc699df4f16d8e3207", 
             "name": "unblockmusic", 
             "tar_url": "unblockmusic/unblockmusic.tar.gz", 
             "title": "解锁网易云灰色歌曲", 
-            "version": "1.1.3"
+            "version": "1.1.4"
         }, 
         {
             "build_date": "2020-05-20_19:44:23", 
--- unblockmusic/config.json.js
+++ unblockmusic/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.1.3",
-"md5":"67402f67050552719232653668e7346b",
+"version":"1.1.4",
+"md5":"e144b391bf6dbabc699df4f16d8e3207",
 "home_url":"Module_unblockmusic.asp",
 "title":"解锁网易云灰色歌曲",
 "description":"解锁网易云灰色歌曲",
-"build_date":"2021-05-08_16:05:27"
+"build_date":"2021-05-18_09:23:31"
 }
